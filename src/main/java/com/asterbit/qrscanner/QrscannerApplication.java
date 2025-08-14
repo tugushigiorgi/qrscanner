2dc457079e672e2e73392318f81e50bc8f88431f
Giorgi <giorgi.tugushi.1@btu.edu.ge>
2025-08-14 18:32:54 +0400

    initial commit

---
+++ src/main/java/com/asterbit/qrscanner/QrscannerApplication.java
@@ -0,0 +1,13 @@
+package com.asterbit.qrscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrscannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrscannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asterbit/qrscanner/QrscannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asterbit.qrscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrscannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
