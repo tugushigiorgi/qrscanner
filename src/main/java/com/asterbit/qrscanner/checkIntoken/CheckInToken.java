39a811cdd539fef187a4c51ef6b958f1b5f5c351
Giorgi <giorgi.tugushi.1@btu.edu.ge>
2025-08-14 21:51:17 +0400

    add redis key

---
+++ src/main/java/com/asterbit/qrscanner/checkIntoken/CheckInToken.java
@@ -0,0 +1,18 @@
+package com.asterbit.qrscanner.checkIntoken;
+
+import jakarta.persistence.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@RedisHash("checkInToken")
+public class CheckInToken implements Serializable {
+
+    @Id
+    private String id;
+    private UUID token;
+    private UUID userId;
+    private UUID classroomId;
+
+}
