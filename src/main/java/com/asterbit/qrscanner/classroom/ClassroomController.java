7af741056c15f8ad5d6571a613f3420a615b686f
Giorgi <giorgi.tugushi.1@btu.edu.ge>
2025-08-15 14:42:03 +0400

    added checkinController

--- src/main/java/com/asterbit/qrscanner/classroom/ClassroomController.java
+++ src/main/java/com/asterbit/qrscanner/classroom/ClassroomController.java
@@ -1,13 +1,14 @@
 package com.asterbit.qrscanner.classroom;
 
+import com.asterbit.qrscanner.checkins.dto.CheckinDto;
 import com.asterbit.qrscanner.classroom.dto.CurrentActivitiesDto;
 import com.asterbit.qrscanner.classroom.service.ClassroomService;
 import com.asterbit.qrscanner.network.RequireSchoolWifi;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -20,14 +21,13 @@ public class ClassroomController {
 
     @GetMapping("/{classroomId}/activities")
     @RequireSchoolWifi
-    public ResponseEntity<CurrentActivitiesDto> currentActivities(HttpServletRequest request, @PathVariable UUID classroomId) {
+    public ResponseEntity<CurrentActivitiesDto> currentActivities(@PathVariable UUID classroomId) {
         return ResponseEntity.ok(classRoomService.currentActivities(classroomId));
     }
 
-
-
-
-
-
-
+    @PostMapping("/{classroomId}/checkin/{activityId}")
+    @RequireSchoolWifi
+    public ResponseEntity<CheckinDto> checkinStudent(@PathVariable UUID classroomId, @PathVariable UUID activityId) {
+        return ResponseEntity.ok(classRoomService.CheckinStudent(classroomId, activityId));
+    }
 }
