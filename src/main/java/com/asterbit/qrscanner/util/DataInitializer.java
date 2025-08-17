309352969e789620867630d6d646b8b66e85d83d
Giorgi <giorgi.tugushi.1@btu.edu.ge>
2025-08-17 21:31:55 +0400

    removed initializer

--- src/main/java/com/asterbit/qrscanner/util/DataInitializer.java
+++ src/main/java/com/asterbit/qrscanner/util/DataInitializer.java
@@ -28,33 +28,33 @@ public class DataInitializer implements CommandLineRunner {
   @Transactional
   @Override
   public void run(String... args) throws Exception {
-    System.out.println("inserting data");
-    var user =User.builder()
-        .email("test@gmail.com")
-        .firstName("test")
-        .lastName("test")
-        .password(passwordEncoder.encode("test123"))
-        .checkins(new HashSet<>())
-        .build();
-    userRepository.save(user);
-
-    for (int i = 1; i <= 3; i++) {
-      var activities = Activity.builder()
-          .title("Introduction to Java - Session " + i)
-          .description("Java basics for classroom " + i)
-          .startTime(LocalDateTime.now().plusHours(24 + i * 2))
-          .endTime(LocalDateTime.now().plusHours(29 + i * 2))
-          .build();
-
-      var newClassroom = Classroom.builder()
-          .name("Classroom " + i)
-          .location("SARTULI " + (2 + i))
-          .activities(new HashSet<>())
-          .build();
-
-      newClassroom.addActivity(activities);
-      activities.setClassroom(newClassroom);
-      classroomRepository.save(newClassroom);
-    }
+//    System.out.println("inserting data");
+//    var user =User.builder()
+//        .email("test@gmail.com")
+//        .firstName("test")
+//        .lastName("test")
+//        .password(passwordEncoder.encode("test123"))
+//        .checkins(new HashSet<>())
+//        .build();
+//    userRepository.save(user);
+//
+//    for (int i = 1; i <= 3; i++) {
+//      var activities = Activity.builder()
+//          .title("Introduction to Java - Session " + i)
+//          .description("Java basics for classroom " + i)
+//          .startTime(LocalDateTime.now().plusHours(24 + i * 2))
+//          .endTime(LocalDateTime.now().plusHours(29 + i * 2))
+//          .build();
+//
+//      var newClassroom = Classroom.builder()
+//          .name("Classroom " + i)
+//          .location("SARTULI " + (2 + i))
+//          .activities(new HashSet<>())
+//          .build();
+//
+//      newClassroom.addActivity(activities);
+//      activities.setClassroom(newClassroom);
+//      classroomRepository.save(newClassroom);
+//    }
   }
 }
