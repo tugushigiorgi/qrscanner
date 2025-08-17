33cff65a86ad10be7139a0f8371486f8e5cf908b
Giorgi <giorgi.tugushi.1@btu.edu.ge>
2025-08-17 19:10:21 +0400

    modified initializer block

--- src/main/java/com/asterbit/qrscanner/util/DataInitializer.java
+++ src/main/java/com/asterbit/qrscanner/util/DataInitializer.java
@@ -4,11 +4,15 @@ import com.asterbit.qrscanner.activity.Activity;
 import com.asterbit.qrscanner.activity.ActivityRepository;
 import com.asterbit.qrscanner.classroom.Classroom;
 import com.asterbit.qrscanner.classroom.ClassroomRepository;
+import com.asterbit.qrscanner.user.User;
+import com.asterbit.qrscanner.user.UserRepository;
+import com.asterbit.qrscanner.user.service.UserService;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @AllArgsConstructor
@@ -16,24 +20,38 @@ public class DataInitializer implements CommandLineRunner {
 
   private final ClassroomRepository classroomRepository;
 
+  private final UserRepository userRepository;
+
+  @Transactional
   @Override
   public void run(String... args) throws Exception {
 
-    var activities = Activity.builder()
-        .title("Activitie 3")
-        .description("Activities are used for testing purposes")
-        .startTime(LocalDateTime.now().plusHours(25))
-        .endTime(LocalDateTime.now().plusHours(30))
+    var user =User.builder()
+        .email("test@gmail.com")
+        .firstName("Giorgi")
+        .lastName("tughushi")
+        .password("test123")
+        .checkins(new HashSet<>())
         .build();
+    userRepository.save(user);
 
+    for (int i = 1; i <= 3; i++) {
+      var activities = Activity.builder()
+          .title("Introduction to Java - Session " + i)
+          .description("Java basics for classroom " + i)
+          .startTime(LocalDateTime.now().plusHours(24 + i * 2))
+          .endTime(LocalDateTime.now().plusHours(29 + i * 2))
+          .build();
 
-    var newclassroom = Classroom.builder()
-        .name("giorgi")
-        .location("New Location 3")
-        .activities(new HashSet<>())
-        .build();
-    newclassroom.addActivity(activities);
-    activities.setClassroom(newclassroom);
-    classroomRepository.save(newclassroom);
+      var newClassroom = Classroom.builder()
+          .name("Classroom " + i)
+          .location("SARTULI " + (2 + i))
+          .activities(new HashSet<>())
+          .build();
+
+      newClassroom.addActivity(activities);
+      activities.setClassroom(newClassroom);
+      classroomRepository.save(newClassroom);
+    }
   }
 }
